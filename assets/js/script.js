976a43c706e43e4cae1c33d2cd7b46139b284660
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-09-29 19:06:18 +0100

    add submit event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,27 @@ const passwordInput = document.querySelector("#password");
 // target the form element
 const formElement = document.querySelector("#sign-up-form");
 
-signUpButton.addEventListener("click", function (event) {
+const onSubmit = function (event) {
   event.preventDefault();
-});
+  console.log("submit");
+
+  // get all data from in put elements
+  const firstName = firstNameInput.value;
+  const lastName = lastNameInput.value;
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  // construct the object in JS to store in LS
+  const userObject = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    password: password,
+  };
+  console.log(userObject);
+
+  // write data into LS
+};
+
+//add submit event listener
+formElement.addEventListener("submit", onSubmit);
