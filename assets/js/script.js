d843d2fc96873b98ddeee548d60d1792fb9a06c5
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-09-29 08:56:16 +0100

    target form input elements

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// target form input elements
+const firstNameInput = document.querySelector("#first-name");
+const lastNameInput = document.querySelector("#last-name");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+
+// target the form element
+const formElement = document.querySelector("#sign-up-form");
+
+signUpButton.addEventListener("click", function (event) {
+  event.preventDefault();
+});
